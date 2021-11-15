c42523f224a343bd200d6487d2dd9a41aaf6cce1
Cassandre Tirions <ctirions@fu-r10-p7.s19.be>
2021-11-15 18:59:43 +0100

    must fix segfaults and too big numbers

--- srcs/actions.c
+++ srcs/actions.c
@@ -6,7 +6,7 @@
 /*   By: ctirions <ctirions@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/08 18:28:56 by ctirions          #+#    #+#             */
-/*   Updated: 2021/11/15 17:04:08 by ctirions         ###   ########.fr       */
+/*   Updated: 2021/11/15 18:30:39 by ctirions         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 static void	slip(t_philos *philo)
 {
-	write_msg(philo, " is sleeping\n");
+	write_msg(philo, "is sleeping\n");
 	pthread_mutex_unlock(&philo->data->forks_m[philo->fork_left]);
 	pthread_mutex_unlock(&philo->data->forks_m[philo->fork_right]);
 	usleep(1000 * philo->data->time_sleep);
@@ -24,7 +24,7 @@ static void	eat(t_philos *philo)
 {
 	philo->is_eating = 1;
 	philo->last_eat = getime();
-	write_msg(philo, " is eating\n");
+	write_msg(philo, "is eating\n");
 	philo->eat_count++;
 	usleep(1000 * philo->data->time_eat);
 	philo->is_eating = 0;
@@ -33,9 +33,9 @@ static void	eat(t_philos *philo)
 static void	take_forks(t_philos *philo)
 {
 	pthread_mutex_lock(&philo->data->forks_m[philo->fork_left]);
-	write_msg(philo, " has taken a fork\n");
+	write_msg(philo, "has taken a fork\n");
 	pthread_mutex_lock(&philo->data->forks_m[philo->fork_right]);
-	write_msg(philo, " has taken a fork\n");
+	write_msg(philo, "has taken a fork\n");
 }
 
 static void	*dead(void *philo_v)
@@ -48,7 +48,7 @@ static void	*dead(void *philo_v)
 		if (!philo->is_eating && \
 		getime() > philo->last_eat + philo->data->time_die)
 		{
-			write_msg(philo, " died\n");
+			write_msg(philo, "died\n");
 			pthread_mutex_unlock(&philo->data->end_m);
 		}
 		usleep(100);
@@ -69,7 +69,7 @@ void	*make_actions(void *philo_v)
 		take_forks(philo);
 		eat(philo);
 		slip(philo);
-		write_msg(philo, " is thinking\n");
+		write_msg(philo, "is thinking\n");
 	}
 	return (NULL);
 }
